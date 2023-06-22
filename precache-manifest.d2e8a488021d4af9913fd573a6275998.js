fdb0ff0a66042c1bc4258a67db25b12f440621cc
prateek272002 <118013154+prateek272002@users.noreply.github.com>
2023-06-22 19:06:29 +0530

    Updates

---
+++ precache-manifest.d2e8a488021d4af9913fd573a6275998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ee05cfe191e3c2fd442",
+    "url": "/twitterclone/static/css/main.409c5fbb.chunk.css"
+  },
+  {
+    "revision": "5ee05cfe191e3c2fd442",
+    "url": "/twitterclone/static/js/main.5ee05cfe.chunk.js"
+  },
+  {
+    "revision": "7bd77854c96d1f8250f1",
+    "url": "/twitterclone/static/js/1.7bd77854.chunk.js"
+  },
+  {
+    "revision": "1757a1207137621c4b71",
+    "url": "/twitterclone/static/js/2.1757a120.chunk.js"
+  },
+  {
+    "revision": "8199be641b72859357cd",
+    "url": "/twitterclone/static/js/runtime~main.8199be64.js"
+  },
+  {
+    "revision": "fedf66cc8e1a0d1298be22bab2fa565a",
+    "url": "/twitterclone/index.html"
+  }
+];
